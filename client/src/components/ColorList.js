715e9034f664b0eea55a90665010fb17d1ea4d8d
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-08-23 12:31:24 -0500

    Sanity Commit

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,22 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      setEditing(false)
+    let newColor = colors.map(el => {
+      return el.id === colorToEdit ? colorToEdit : el
+    });
+    updateColors(newColor)
+    })
+    window.location.reload();
   };
 
   const deleteColor = color => {
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(res => console.log(res))
     // make a delete request to delete this color
+    window.location.reload();
   };
 
   return (
